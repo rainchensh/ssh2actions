3fc65cd062d4d4113f580e4bb6171dab45435fd1
rainchensh <81663884+rainchensh@users.noreply.github.com>
2025-01-05 20:03:03 +0800

    Update tmate2actions.sh
    del telegram

--- tmate2actions.sh
+++ tmate2actions.sh
@@ -58,21 +58,6 @@ ${TMATE_WEB}
 Run '\`touch ${CONTINUE_FILE}\`' to continue to the next step.
 "
 
-if [[ -n "${TELEGRAM_BOT_TOKEN}" && -n "${TELEGRAM_CHAT_ID}" ]]; then
-    echo -e "${INFO} Sending message to Telegram..."
-    curl -sSX POST "${TELEGRAM_API_URL:-https://api.telegram.org}/bot${TELEGRAM_BOT_TOKEN}/sendMessage" \
-        -d "disable_web_page_preview=true" \
-        -d "parse_mode=Markdown" \
-        -d "chat_id=${TELEGRAM_CHAT_ID}" \
-        -d "text=${MSG}" >${TELEGRAM_LOG}
-    TELEGRAM_STATUS=$(cat ${TELEGRAM_LOG} | jq -r .ok)
-    if [[ ${TELEGRAM_STATUS} != true ]]; then
-        echo -e "${ERROR} Telegram message sending failed: $(cat ${TELEGRAM_LOG})"
-    else
-        echo -e "${INFO} Telegram message sent successfully!"
-    fi
-fi
-
 while ((${PRT_COUNT:=1} <= ${PRT_TOTAL:=10})); do
     SECONDS_LEFT=${PRT_INTERVAL_SEC:=10}
     while ((${PRT_COUNT} > 1)) && ((${SECONDS_LEFT} > 0)); do
